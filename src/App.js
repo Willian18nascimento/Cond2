6cc6a9e490a45aac6d30cb3beb0f76d264894046
Willian18nascimento <36906632+Willian18nascimento@users.noreply.github.com>
2021-10-30 03:57:42 -0300

    Rev 2 Alteração 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Aula UFRA d 1</h1>
+      <h1> Sistema de Cadastro Empresa</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
